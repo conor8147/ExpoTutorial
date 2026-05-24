f9225da4db019018546263280d9fece811bf03e9
Conor O'Neill <coneill@atlassian.com>
2026-05-25 09:36:13 +1000

    mucking around with about screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { Link, useNavigation } from "expo-router";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const navigation = useNavigation();
+
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>
+        Edit app/index.tsx to edit this screen.
+      </Text>
+
+      <Link href="/about" style={styles.button}>
+        Go to about screen
+      </Link>
+      
+      <Button 
+      title="Go to about screen"
+      onPress={() => navigation.navigate('about')} 
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff'
+  },
+    button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+})
